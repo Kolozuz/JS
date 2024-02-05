d743748829bcd0b0e915c3699092f29046544184
Juan Pablo Morales Cruz <110570465+Kolozuz@users.noreply.github.com>
2024-02-05 12:23:01 -0500

    Update winget.list.ps1
    - Add Retroarch, definitive emulation tool
    >[!NOTE]
    >I struggled deciding whether to add RetroArch or to add each individual emulator, there's upsides and downsides, I ended up just going with RetroArch as I think overall the pros outweigh the cons, with the biggest cons being the complicated interface and setup, and lower performance on some emus, and the pros being the ability to have all your emus in one place, integration with RetroAchivements, multiplayer features, just to mention some. 
    That being said here's the list of recommended emus/cores to use with RetroArch, based mainly on ease of use, active development and community:
        - mGBA - GameBoy and GameBoy Advanced
        - snes9x - SNES
        - DuckStation - PlayStation 1
        - PSCX2 - PlayStation 2
        - Dolphin - Gamcube / Wii
        - MelonDS - Nintendo DS
        - PPSSPP - PSP
        - MAME - Arcade
    - Add GeforceNow, currently best option for cloud gaming
    - Remove some specific things, for i want to keep the list with just the bare essentials for gaming/emulating/coding

--- .config/pwshUtils/winget.list.ps1
+++ .config/pwshUtils/winget.list.ps1
@@ -1,68 +1,44 @@
-# Esto existe por si falla el import de winget
 $AplicationsList = @(
-	# "Git.Git"
 	# Tools
+	"Git.Git"
 	"GitHub.GitHubDesktop"
-	"Docker.DockerDesktop"
-	"dbeaver.dbeaver"
 	"Insomnia.Insomnia"
-	"Microsoft.XNARedist"
-	# Console Emulators
+	#"Microsoft.XNARedist"
+	# Terminal
 	"Microsoft.WindowsTerminal"
-	"Mobatek.MobaXterm"
 	# Cli
 	"Microsoft.PowerShell"
 	"GitHub.cli"
-	"BurntSushi.ripgrep.MSVC"
-	"sharkdp.fd"
 	# Editores de texto y IDEs
-	"Microsoft.VisualStudioCode.Insiders"
 	"Microsoft.VisualStudioCode"
 	"Neovim.Neovim"
-	"Google.AndroidStudio"
-	"VSCodium.VSCodium"
 	# Lenguajes de programación
 	"CoreyButler.NVMforWindows"
 	"Python.Python.3.11"
 	"Rustlang.Rustup"
-	"GoLang.Go"
-	"zig.zig"
 	"rjpcomputing.luaforwindows"
 	"Oracle.JavaRuntimeEnvironment"
-	"RubyInstallerTeam.Ruby.3.1"
 	# SDKs
-	"Microsoft.WindowsSDK.10.0.22000"
 	"Oracle.JDK.18"
 	# Design y multimedia
 	"OBSProject.OBSStudio"
 	"GIMP.GIMP"
 	"Microsoft.PowerToys"
-	# Navegadores
-	"Mozilla.Firefox.DeveloperEdition"
 	# Games
 	"Valve.Steam"
+	"Nvidia.GeForceNow"
+	# Emulation
+	"Libretro.RetroArch"
 	# Remote
-	"GlavSoft.TightVNC"
-	"AnyDeskSoftwareGmbH.AnyDesk"
+	"Parsec.Parsec"
+	"LogMeIn.Hamachi"
 	# Social
 	"Discord.Discord"
-	"Telegram.TelegramDesktop"
-	# Cloud
-	"Microsoft.OneDrive"
-	# Visual Studio
-	"Microsoft.VCRedist.2015+.x64"
-	"Microsoft.VisualStudio.2022.Community"
-	"Microsoft.VisualStudio.2022.BuildTools"
 	# Security
 	"Twilio.Authy"
 	"RonyShapiro.PasswordSafe"
-	# Opcional
-	"Highresolution.X-MouseButtonControl"
-	"Logitech.OptionsPlus"
-	"Kingston.SSDManager"
-	# productivity and office
+	# Productivity
 	"AnyAssociation.Anytype"
-	"Logseq.Logseq"
 )
 
 $AplicationsList | ForEach-Object {
